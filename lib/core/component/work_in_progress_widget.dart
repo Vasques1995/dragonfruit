f77cde479f7bfced450c7effc48b9b2821d20998
Rodrigo Liporace <rodrigo.vasquestuf@gmail.com>
2021-08-04 23:33:45 -0300

    🐲[FEATURE](pubspec): Added beholder assets
    Used for WorkInProgressWidget

---
+++ lib/core/component/work_in_progress_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class WorkInProgressWidget extends StatefulWidget {
+  WorkInProgressWidget({Key? key, this.message, this.messageTextStyle})
+      : super(key: key);
+  final String? message;
+  final TextStyle? messageTextStyle;
+
+  @override
+  _WorkInProgressWidgetState createState() => _WorkInProgressWidgetState();
+}
+
+class _WorkInProgressWidgetState extends State<WorkInProgressWidget> {
+  @override
+  Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'gifs/beholder_gif.gif',
+          width: deviceSize.width / 2,
+          fit: BoxFit.fitWidth,
+        ),
+        if (widget.message != null)
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Text(
+              widget.message!,
+              style: widget.messageTextStyle,
+            ),
+          )
+      ],
+    );
+  }
+}
